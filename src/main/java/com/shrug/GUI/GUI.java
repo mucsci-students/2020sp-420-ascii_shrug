aa4c5ddd2c8a54bdedfa213a3106eb8ae27dec98
Austin <ausjamesmoore@gmail.com>
2020-02-23 19:42:45 -0500

    added diagram pane

--- src/main/java/com/shrug/GUI/GUI.java
+++ src/main/java/com/shrug/GUI/GUI.java
@@ -1,43 +1,76 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import Controller.*;
 
 public class GUI extends Application {
   private Button add, remove, edit, save, load;
 
   public GUI() {}
 
-  /*public static void main(String[] args) {
-      launch(Args);
-  }*/
-
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("GUI");
 
-    BorderPane root = new BorderPane();
-    Scene scene = new Scene(root, 600, 600);
+    BorderPane borderPane = new BorderPane();
 
+    Scene scene = new Scene(borderPane, 600, 600);
+
+    initDiagram(borderPane);
+    initButtons(borderPane);
+
+    borderPane.setMargin(borderPane.getCenter(), new Insets(30));
+
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public void initDiagram (BorderPane borderPane) {
+    StackPane diagram = new StackPane();
+    diagram.setStyle("-fx-background-color: antiquewhite;");
+    diagram.setStyle("-fx-border-style: solid;");
+    diagram.setStyle("-fx-border-color: black;");
+    borderPane.setCenter(diagram);
+  }
+
+  public void initButtons (BorderPane borderPane) {
     add = new Button("Add");
+    add.setOnAction(this::processButtonPressAdd);
+
     remove = new Button("Remove");
+    remove.setOnAction(this::processButtonPressRemove);
+
     load = new Button("Load");
+    load.setOnAction(this::processButtonPressLoad);
+
     save = new Button("Save");
+    save.setOnAction(this::processButtonPressSave);
+
     edit = new Button("Edit");
+    edit.setOnAction(this::processButtonPressEdit);
 
     FlowPane options = new FlowPane(add, remove, load, save, edit);
     options.setAlignment(Pos.CENTER);
     options.setHgap(10);
-
-    root.setTop(options);
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    borderPane.setTop(options);
   }
+
+  public void processButtonPressAdd(ActionEvent event) {}
+
+  public void processButtonPressRemove(ActionEvent event) {}
+
+  public void processButtonPressLoad(ActionEvent event) {}
+
+  public void processButtonPressSave(ActionEvent event) {}
+
+  public void processButtonPressEdit(ActionEvent event) {}
 }
 
 /*public class GUI {
