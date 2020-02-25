96a2f2118313f4b9e956b6f62a73772710ffa70f
Austin <ausjamesmoore@gmail.com>
2020-02-25 14:59:19 -0500

    creating to components to add diagram

--- src/main/java/com/shrug/GUI/GUI.java
+++ src/main/java/com/shrug/GUI/GUI.java
@@ -8,15 +8,30 @@ import javafx.scene.control.Button;
 import javafx.scene.paint.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
 import Controller.*;
+
+import com.mxgraph.layout.*;
+import com.mxgraph.swing.*;
+import javax.swing.*;
+import javafx.embed.swing.*;
+//import java.awt.*;
+import org.jgrapht.*;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.*;
+import shrugUML.*;
 
 public class GUI extends Application {
   private Button add, remove, edit, save, load;
+  SimpleDirectedGraph test = new SimpleDirectedGraph<ShrugUMLClass, DefaultEdge>(DefaultEdge.class);
+  private JGraphXAdapter<ShrugUMLClass, DefaultEdge> jgxAdapter = new JGraphXAdapter<ShrugUMLClass, DefaultEdge> (test);
+  mxIGraphLayout layout = new mxOrganicLayout(jgxAdapter);
 
   public GUI() {}
 
   @Override
   public void start(Stage primaryStage) {
+
     primaryStage.setTitle("GUI");
 
     BorderPane borderPane = new BorderPane();
@@ -24,12 +39,26 @@ public class GUI extends Application {
     Scene scene = new Scene(borderPane, 600, 600);
 
     initDiagram(borderPane);
-    initButtons(borderPane);
+    initMenuBar(borderPane);
+    //initButtons(borderPane);
 
     borderPane.setMargin(borderPane.getCenter(), new Insets(30));
 
     primaryStage.setScene(scene);
     primaryStage.show();
+  }
+
+  public void createSwingDiagram() {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        SwingNode diagramNode = new SwingNode();
+        JInternalFrame frame = new JInternalFrame();
+        layout.execute(jgxAdapter.getDefaultParent());
+        frame.add(new mxGraphComponent(jgxAdapter));
+        diagramNode.setContent(frame);
+      }
+    });
   }
 
   public void initDiagram (BorderPane borderPane) {
@@ -38,6 +67,33 @@ public class GUI extends Application {
     diagram.setStyle("-fx-border-style: solid;");
     diagram.setStyle("-fx-border-color: black;");
     borderPane.setCenter(diagram);
+  }
+
+  public void initMenuBar (BorderPane borderPane) {
+    final Menu file = new Menu("File");
+    final Menu help = new Menu("Help");
+    final Menu edit = new Menu("Edit");
+
+    MenuBar menuBar = new MenuBar();
+    menuBar.getMenus().add(file);
+    menuBar.getMenus().add(help);
+    menuBar.getMenus().add(edit);
+
+    MenuItem add = new MenuItem("Add");
+    add.setOnAction(this::processButtonPressAdd);
+    MenuItem remove = new MenuItem("Remove");
+    remove.setOnAction(this::processButtonPressRemove);
+    MenuItem save = new MenuItem("Save");
+    save.setOnAction(this::processButtonPressSave);
+    MenuItem load = new MenuItem("Load");
+    load.setOnAction(this::processButtonPressLoad);
+
+    file.getItems().add(add);
+    file.getItems().add(remove);
+    file.getItems().add(save);
+    file.getItems().add(load);
+
+    borderPane.setTop(menuBar);
   }
 
   public void initButtons (BorderPane borderPane) {
