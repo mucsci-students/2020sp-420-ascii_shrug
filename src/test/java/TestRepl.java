682af309c782d2847aa0d5bd7ec79d9aeab3fc28
jfaz92 <31872393+jfaz92@users.noreply.github.com>
2020-04-05 20:58:00 -0400

    Implement Repl tests

--- src/test/java/TestRepl.java
+++ src/test/java/TestRepl.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.*;
+import Command.*;
 import org.junit.*;
 import shrugUML.*;
 import com.shrug.Repl.*;
@@ -21,6 +22,32 @@ public class TestRepl {
     assertEquals(Repl.parseLine (testCmd), test);
   }
 
+  @Test
+  public void testParseAttributes() {
+    ArrayList<String> test = new ArrayList<String>(Arrays.asList("add", "classA", "-a", "int", "a"));
 
+    assertEquals(Arrays.asList("int a"), Repl.parseAttributes(test).getFields());
+  }
+
+  @Test
+  public void testParseRelationships() {
+    ArrayList<String> test = new ArrayList<String>(Arrays.asList("add", "classA", "-r", "Aggregation", "classB"));
+    HashMap<String, RType> rels = new HashMap<String, RType> ();
+    rels.put ("classB", RType.Aggregation);
+
+    assertEquals(rels, Repl.parseRelationships(test));
+  }
+
+  @Test
+  public void testBuild() {
+    ArrayList<String> test = new ArrayList<String>(Arrays.asList("add", "classA", "-a", "int", "a", "-r", "Aggregation", "classB"));
+
+    HashMap<String, RType> rels = new HashMap<String, RType> ();
+    rels.put ("classB", RType.Aggregation);
+
+    Command command = Repl.build(test);
+    assertEquals(Arrays.asList("int a"), command.getFields());
+    assertEquals(rels, command.getRelationships());
+  }
 }
 
